85d06f5c6ceaa99a1a19387065cb1c79edd8c9b4
GyewanKim <87048326+larpp@users.noreply.github.com>
2024-04-06 23:57:28 +0900

    Delete tools/imbalance_resnet50.sh

--- tools/imbalance_resnet50.sh
+++
@@ -1,5 +0,0 @@
-python resnet50.py --img_path img_im_data1 --save_model results_resnet50_im1.pt
-python resnet50.py --img_path img_im_data2 --save_model results_resnet50_im2.pt
-python resnet50.py --img_path img_im_data3 --save_model results_resnet50_im3.pt
-python resnet50.py --img_path img_im_data4 --save_model results_resnet50_im4.pt
-python resnet50.py --img_path img_im_data5 --save_model results_resnet50_im5.pt
